8b6ba923f8f8200df3e11573b6a5b70d949bcfcb
Harsh Mavi <harshmavi2001@gmail.com>
2022-10-29 00:16:00 +0530

    chef and IPC certificates

---
+++ IPCcertificate.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int fun(int n, int m,int k)
+{
+    int j,q,sum=0,arr[k];
+    for(j=0;j<k;j++)
+	    {
+	        cin>>arr[j];
+	    }
+	    cin>>q;
+	    for(j=0;j<k;j++)
+	    {
+	        sum = sum+arr[j];
+	    }
+	    if(q<=10 && sum>=m)
+	    {
+	        return 1;
+	    }
+	    else
+	    return 0;
+	    
+	    
+}
+int main() {
+	int i,j,n,m,k,count=0;
+	cin>>n>>m>>k;
+	for(i=0;i<n;i++){
+	    if(fun(n,m,k)==1)
+	    {
+	        count=count+1;
+	    }
+	}
+	cout<<count;
+	return 0;
+}
