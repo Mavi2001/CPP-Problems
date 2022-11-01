607635bf8b9c59fc3c82d4c644d8d9948f46e560
Harsh Mavi <harshmavi2001@gmail.com>
2022-11-01 23:19:50 +0530

    burgers

---
+++ burger.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i,t,a,b;
+	cin>>t;
+	for(i=0;i<t;i++)
+	{
+	    cin>>a>>b;
+	    if(a<=b)
+	    {
+	        cout<<a<<endl;
+	    }
+	    else
+	    cout<<b<<endl;
+	}
+	return 0;
+}
