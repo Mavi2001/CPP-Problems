19e380cb7852101556e9602cb2ecca32513b8a05
Harsh Mavi <harshmavi2001@gmail.com>
2022-11-02 23:43:45 +0530

    Avoid Squares Please

---
+++ squares.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i,t,n,j;
+	cin>>t;
+	for(i=0;i<t;i++)
+	{
+	    cin>>n;
+	    for(j=0;j<n;j++)
+	    {
+	        cout<<j+1<<" ";
+	    }
+	    cout<<endl;
+	}
+	return 0;
+}
