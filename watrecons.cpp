35c5282a8bc3f66cc9fc8f03ee89b800ad2334bf
Harsh Mavi <harshmavi2001@gmail.com>
2022-11-09 23:50:12 +0530

    water conservation

---
+++ watrecons.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i,t,n;
+	cin>>t;
+	for(i=0;i<t;i++)
+	{
+	    cin>>n;
+	    if(n>=2000)
+	    cout<<"YES"<<endl;
+	    else
+	    cout<<"NO"<<endl;
+	}
+	return 0;
+}
