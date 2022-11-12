1f395438fec3f64f612d31e95516e155808c3263
Harsh Mavi <harshmavi2001@gmail.com>
2022-11-12 22:29:35 +0530

    Time complexity

---
+++ complexity.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i,t,x,y;
+	cin>>t;
+	for(i=0;i<t;i++)
+	{
+	    cin>>x>>y;
+	    if(x>y)
+	    cout<<"YES"<<endl;
+	    else
+	    cout<<"NO"<<endl;
+	}
+	return 0;
+}
