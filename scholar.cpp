5641be6891c269b1f0680738a5ab3010f202b164
Harsh Mavi <harshmavi2001@gmail.com>
2022-11-24 19:17:47 +0530

    scholarship

---
+++ scholar.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i,t,n;
+	
+	    cin>>n;
+	    if(n>=1 && n<=50)
+	    cout<<"100"<<endl;
+	    else if(n>50 && n<=100)
+	    cout<<"50"<<endl;
+	    else
+	    cout<<"0"<<endl;
+	
+	return 0;
+}
