a6659d63985709086740c242a2a12da285541650
Harsh Mavi <harshmavi2001@gmail.com>
2022-11-25 22:27:25 +0530

    date with girlfriend

---
+++ date.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,x,y;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>x>>y;
+	    if(x<y)
+	    cout<<"NO"<<endl;
+	    else
+	    cout<<"YES"<<endl;
+	}
+	return 0;
+}
