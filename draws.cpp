e9f7a5a2a90f66a7cb56e0b935d94ecb369a746f
Harsh Mavi <harshmavi2001@gmail.com>
2022-12-16 18:55:09 +0530

    lockout draws

---
+++ draws.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,a,b,c;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>a>>b>>c;
+	    if(a+b == c || b+c == a || a+c == b)
+	    cout<<"YES"<<endl;
+	    else
+	    cout<<"NO"<<endl;
+	}
+	return 0;
+}
